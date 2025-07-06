d08a38349c99297e76d19a8fb8b45ea3101a1587
s01ix <michal8069@wp.pl>
2025-07-06 17:19:48 +0200

    Configure Project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.github.s01ix.organiser {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.github.s01ix.organiser to javafx.fxml;
+    exports org.github.s01ix.organiser;
+}
